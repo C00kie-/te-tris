3f7328bbab4a44c6d4f221047eb2b7a7a3be8358
Pauline BOURMEAU <pbourmea@e3r5p16.42.fr>
2017-02-09 12:24:16 +0100

    en objet svp !

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
 #python 2.7
 #initialisation de ma grille de jeu
 
-grid = [[]]
-
-x, y = 10, 12
-
-grid = [[0 for i in xrange(x)] for i in xrange(y)]
+class Maps:
+	def __init__(self, name, x, y):
+		self.map = []
 
 
-#print de ma grille de jeu
-for i in xrange(x):
-	print grid[i]
+class Grid:
+	def __init__(self, name, x, y):
+		self.name = name
+		self.x = x
+		self.y = y
+		self.map = [[]]
+		self.map = [[0 for i in xrange(x)] for i in xrange(y)]#writes Zeros at each position in the map
+
+	def display(self):
+
+		for i in xrange(self.x):
+			print self.map[i]
+
+firstMap = Grid("Alabama", 10, 12)
+
+secondMap = Grid("Texas", 15, 30)
+
+thirdMap = Grid("Montana", 3, 5)
+
+print firstMap.name
+
+firstMap.display()
+
+
+
+print secondMap.name
+
+secondMap.display()
+
+
+
+print thirdMap.name
+
+thirdMap.display()
+
+
+
+
+	#print de ma grille de jeu
+#for i in xrange(grid.x):
+#	print grid.map[i]
