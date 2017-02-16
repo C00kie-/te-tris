38c389eae09b1728e95f946fcfa9feae8f58874a
Pauline BOURMEAU <pbourmea@e3r5p14.42.fr>
2017-02-16 11:55:34 +0100

    manipulating instances

--- main.py
+++ main.py
@@ -12,35 +12,125 @@ class Grid:
 		self.x = x
 		self.y = y
 		self.map = [[]]
-		self.map = [[0 for i in xrange(x)] for i in xrange(y)]#writes Zeros at each position in the map
+		self.map = [[ 0 for i in xrange(x)] for i in xrange(y)]#writes Zeros at each position in the map
 
 	def display(self):
 
 		for i in xrange(self.x):
 			print self.map[i]
 
-firstMap = Grid("Alabama", 10, 12)
-
-secondMap = Grid("Texas", 15, 30)
-
-thirdMap = Grid("Montana", 3, 5)
-
-print firstMap.name
-
-firstMap.display()
+	def apply_gravity(self):
+		print("\napply_gravity()")
 
 
+class Piece:
+	def __init__(self, x, y, shape):
+		self.current_coords = []##comment je represente ma piece ?
 
-print secondMap.name
+	def move_piece(self):
+		print("\nmove_piece()")
+	# def move_piece_down():
+	#
+	# def move_grid_down():
+	#
+	def checkLines(self):
+		game.status = 4
+	#
+	# def disolve_piece():
+	#
+	def generate_new(self):
+		print("generate_new_piece()")
 
-secondMap.display()
+
+#for mainloop uses
+class Game:
+	def __init__(self):
+		self.status = 1;
+		self.ticket_counter = 0
+
+
+class Player:
+	def __init__(self, id):
+		self.id = 1
+
+	def get_controler(self): ## input
+		print("\nget_controler()")
+
+
+###########test bloc#######################################################
+game = Game()
+piece = Piece(1,6,1)
+grid = Grid("example", 10,12)
+player = Player(1)
+
+print(game)
+print(game.status)
+
+
+def mainLoop():
+	# while game.status != 0:
+	# 	print(game.status)
+	# 	game.status = 0
+	#
+	# if game.status == 0:
+	# 	print("\nit's done ")
+	while game.status != 0:
+		game.ticket_counter += 1
+		print("ticket counter : ", game.ticket_counter)
+
+		if game.status == 0:
+			piece.generate_new()
+			game.status = 1
+
+		elif game.status == 1:
+			player.get_controler()
+			grid.apply_gravity() ## if tick counter > 60%
+			piece.move_piece()
+			game.status = 2
+			print(game.status)
+
+		elif game.status == 2:
+			piece.checkLines()
+			print(game.status)
+
+
+		elif game.status == 4:
+			player.get_controler()
+			game.status = 0
+			print(game.status)
+
+
+	piece.generate_new()
 
 
 
-print thirdMap.name
 
-thirdMap.display()
+mainLoop()
 
+############################################################################
+#
+# firstMap = Grid("Alabama", 10, 12)
+#
+# secondMap = Grid("Texas", 15, 30)
+#
+# thirdMap = Grid("Montana", 3, 5)
+#
+# print firstMap.name
+#
+# firstMap.display()
+#
+#
+#
+# print secondMap.name
+#
+# secondMap.display()
+#
+#
+#
+# print thirdMap.name
+#
+# thirdMap.display()
+#
 
 
 
